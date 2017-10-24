123a64197669a6db442fdad307fd3b41d7d00531
Luca Del Puppo <luca@delpuppo.net>
2017-10-25 01:21:38 +0200

    Add newMessage and createMessage event

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect', function() {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    to: 'Luca',
+    text: 'Yup, that work for me.'
+  });
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+  console.log('New Message', message);
+});
+
