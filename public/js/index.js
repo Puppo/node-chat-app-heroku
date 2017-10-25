35aa99264ac64d8c82dd8015a5f9930723379ecb
Luca Del Puppo <luca@delpuppo.net>
2017-10-25 09:58:39 +0200

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,20 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-  console.log('New Message', message);
   const li = $('<li></li>');
   li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function(message) {
+  const a = $('<a></a>')
+            .text('My current location')
+            .attr('target', '_blank')
+            .attr('href', message.url);
+  const li = $('<li></li>')
+            .text(`${message.from}: `)
+            .append(a);
 
   $('#messages').append(li);
 });
@@ -19,10 +30,48 @@ socket.on('newMessage', function(message) {
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  const txtMessage =  $('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: txtMessage.val()
   }, function() {
+    txtMessage.val(null);
+  });
+});
 
+const btnLocation = $('#send-location');
+btnLocation.on('click', function(e) {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  let bestPosition, geolocationWatchID;
+
+  const fetchLocation = function(position) {
+    if (!bestPosition || bestPosition.coords.accuracy > position.coords.accuracy) {
+      bestPosition = position;
+    }
+
+    // wifi problem get accuracy geolocation
+    if (bestPosition.coords.accuracy <= 40000) {
+      navigator.geolocation.clearWatch(geolocationWatchID);
+      socket.emit('createLocationMessage', {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });
+    }
+  };
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    bestPosition = position;
+    var geo_options = {
+      enableHighAccuracy: true, 
+      maximumAge        : 30000, 
+      timeout           : 27000
+    };
+    geolocationWatchID = navigator.geolocation.watchPosition(fetchLocation, null, geo_options);
+  }, function(e) {
+    alert('Unable to fetch location.');
   });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,15 +1,29 @@
 
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
-  it('should genereta correct message object', () => {
+  it('should generete correct message object', () => {
     const from = 'example'
         , text = 'My text';
     const message = generateMessage(from, text);
     expect(message).toMatchObject({from, text});
     expect(message.createAt).toBeTruthy();
     expect(typeof message.createAt).toBe('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location message object', () => {
+    const from = 'Deb'
+          , latitude = 15
+          , longitude = 16
+          , url = 'https://www.google.com/maps?q=15,16';
+
+    const locationMessage = generateLocationMessage(from, latitude, longitude);
+    expect(locationMessage).toMatchObject({from, url});
+    expect(locationMessage.createAt).toBeTruthy();
+    expect(typeof locationMessage.createAt).toBe('number');
   });
 });
